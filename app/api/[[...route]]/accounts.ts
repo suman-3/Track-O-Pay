1fee8cd14c70040671ddf87daf76583feebaa1b2
Suman Mondal <mondalsumanrock20@gmail.com>
2024-05-15 01:29:39 +0530

    changing package name

--- app/api/[[...route]]/accounts.ts
+++ app/api/[[...route]]/accounts.ts
@@ -4,6 +4,7 @@ import { db } from "@/db/drizzle";
 import { accounts } from "@/db/schema";
 import { clerkMiddleware, getAuth } from "@hono/clerk-auth";
 import { HTTPException } from "hono/http-exception";
+import { eq } from "drizzle-orm";
 
 const app = new Hono().get("/", clerkMiddleware(), async (c) => {
   const auth = getAuth(c);
@@ -19,7 +20,9 @@ const app = new Hono().get("/", clerkMiddleware(), async (c) => {
       id: accounts.id,
       name: accounts.name,
     })
-    .from(accounts);
+    .from(accounts)
+    .where(eq(accounts.userId, auth.userId));
+
   return c.json({ data });
 });
 
