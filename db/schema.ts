b9523f7deb0701b4333c80c8087386cfc3e8aefa
Suman Mondal <mondalsumanrock20@gmail.com>
2024-05-15 01:57:13 +0530

    Some required packages installed

--- db/schema.ts
+++ db/schema.ts
@@ -1,4 +1,5 @@
 import { pgTable, text } from "drizzle-orm/pg-core";
+import {createInsertSchema} from "drizzle-zod"
 
 export const accounts = pgTable("accounts", {
   id: text("id").primaryKey(),
@@ -7,4 +8,6 @@ export const accounts = pgTable("accounts", {
   userId: text("user_id").notNull(),
 });
 
+
+export const insertAccountSchema = createInsertSchema(accounts);
 
