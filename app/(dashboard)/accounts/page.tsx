b4c6b4f0fb5a3016904a0b94630294d3c4842d55
Suman Mondal <mondalsumanrock20@gmail.com>
2024-05-15 18:37:15 +0530

    Acconts endpoint added to data table and loader add to table

--- app/(dashboard)/accounts/page.tsx
+++ app/(dashboard)/accounts/page.tsx
@@ -3,34 +3,37 @@
 import { userNewAccount } from "@/features/accounts/hooks/use-new-account";
 import { Button } from "@/components/ui/button";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
-import { CirclePlus } from "lucide-react";
+import { CirclePlus, Loader2 } from "lucide-react";
 import React from "react";
-
-import { Payment, columns } from "./column";
-
 import { DataTable } from "@/components/layout/data-table";
-
-const data: Payment[] = [
-  {
-    id: "728ed52f",
-    amount: 100,
-    status: "pending",
-    email: "m@example.com",
-  },
-  {
-    id: "728ed52f",
-    amount: 50,
-    status: "pending",
-    email: "a@example.com",
-  },
-  // ...
-];
+import { columns } from "./column";
+import { useGetAccounts } from "@/features/accounts/api/use-get-accounts";
+import { Skeleton } from "@/components/ui/skeleton";
 
 const AccountsPage = () => {
   const newAccount = userNewAccount();
+  const accountsQuery = useGetAccounts();
+  const accounts = accountsQuery.data || [];
+
+  if (accountsQuery.isLoading) {
+    return (
+      <div className="max-w-screen-2xl mx-auto w-full pb-10 -mt-14 md:-mt-20 lg:-mt-[7rem]">
+        <Card className="drop-shadow-sm border-none">
+          <CardHeader>
+            <Skeleton className="h-8 w-48 " />
+          </CardHeader>
+          <CardContent>
+            <div className="h-[300px] w-full flex items-center justify-center">
+              <Loader2 className="size-10 text-slate-300 animate-spin" />
+            </div>
+          </CardContent>
+        </Card>
+      </div>
+    );
+  }
   return (
     <div className="max-w-screen-2xl mx-auto w-full pb-10 -mt-14 md:-mt-20 lg:-mt-[7rem]">
-      <Card className="drop-shadow-sm">
+      <Card className="drop-shadow-sm border-none">
         <CardHeader className="gap-y-2 flex flex-row items-center justify-between px-5 py-3">
           <CardTitle className="text-xl line-clamp-1">Account page</CardTitle>
           <Button size="sm" onClick={newAccount.onOpen}>
@@ -42,7 +45,7 @@ const AccountsPage = () => {
           <DataTable
             filterKey="email"
             columns={columns}
-            data={data}
+            data={accounts}
             onDelete={() => {}}
             disabled={false}
           />
