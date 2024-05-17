58bb61eaf997030d75342d2cc3bbbbeda2efccce
Suman Mondal <mondalsumanrock20@gmail.com>
2024-05-17 16:13:39 +0530

    Tracsaction HONO route created

--- app/api/[[...route]]/route.ts
+++ app/api/[[...route]]/route.ts
@@ -2,6 +2,7 @@ import { Hono } from "hono";
 import { handle } from "hono/vercel";
 import accounts from "./accounts";
 import categories from "./categories"
+import transactions from "./transactions"
 
 export const runtime = "edge";
 
@@ -10,6 +11,7 @@ const app = new Hono().basePath("/api");
 const routes = app
 .route("/accounts", accounts)
 .route("/categories", categories)
+.route("/transactions", transactions)
 
 export const GET = handle(app);
 export const POST = handle(app);
