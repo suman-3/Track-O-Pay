e70b38c4780e7b0e70690f40f7399bee8fff7e3a
Suman Mondal <mondalsumanrock20@gmail.com>
2024-05-17 21:01:32 +0530

    Transaction data listed in column

--- app/(dashboard)/transactions/column.tsx
+++ app/(dashboard)/transactions/column.tsx
@@ -7,6 +7,9 @@ import { ArrowUpDown } from "lucide-react";
 import { InferResponseType } from "hono";
 import { client } from "@/lib/hono";
 import { Actions } from "./actions";
+import { format } from "date-fns";
+import { formatCurrency } from "@/lib/utils";
+import { Badge } from "@/components/ui/badge";
 
 export type ResponseType = InferResponseType<
   typeof client.api.transactions.$get,
@@ -47,9 +50,89 @@ export const columns: ColumnDef<ResponseType>[] = [
         </Button>
       );
     },
+    cell: ({ row }) => {
+      const date = row.getValue("date") as Date;
+      return <span>{format(date, "dd MMMM, yyyy")}</span>;
+    },
+  },
+  {
+    accessorKey: "category",
+    header: ({ column }) => {
+      return (
+        <Button
+          variant="ghost"
+          onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
+        >
+          Category
+          <ArrowUpDown className="ml-2 h-4 w-4" />
+        </Button>
+      );
+    },
+    cell: ({ row }) => {
+      return <span>{row.original.category}</span>;
+    },
+  },
+  {
+    accessorKey: "payee",
+    header: ({ column }) => {
+      return (
+        <Button
+          variant="ghost"
+          onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
+        >
+          Payee
+          <ArrowUpDown className="ml-2 h-4 w-4" />
+        </Button>
+      );
+    },
+  },
+  {
+    accessorKey: "amount",
+    header: ({ column }) => {
+      return (
+        <Button
+          variant="ghost"
+          onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
+        >
+          Amount
+          <ArrowUpDown className="ml-2 h-4 w-4" />
+        </Button>
+      );
+    },
+    cell: ({ row }) => {
+      const amount = parseFloat(row.getValue("amount"));
+      return (
+        <Badge
+          variant={amount < 0 ? "destructive" : "primary"}
+          className="text-sm font-medium px-3 py-1.5"
+        >
+          {formatCurrency(amount)}
+        </Badge>
+      );
+    },
+  },
+  {
+    accessorKey: "account",
+    header: ({ column }) => {
+      return (
+        <Button
+          variant="ghost"
+          onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
+        >
+          Account
+          <ArrowUpDown className="ml-2 h-4 w-4" />
+        </Button>
+      );
+    },
+    cell: ({ row }) => {
+      return <span>{row.original.account}</span>;
+    },
   },
   {
     id: "actions",
+    header: ({ column }) => {
+      return <span>Actions</span>;
+    },
     cell: ({ row }) => <Actions id={row.original.id} />,
   },
 ];
--- lib/utils.ts
+++ lib/utils.ts
@@ -6,9 +6,20 @@ export function cn(...inputs: ClassValue[]) {
 }
 
 export function convertAmountFromMiliunits(amount: number) {
-  return amount / 1000;
+  return amount;
+  // return amount / 1000;
 }
 
 export function convertAmountToMiliunits(amount: number) {
-  return Math.round(amount * 1000);
+  // return Math.round(amount * 1000);
+  return Math.round(amount);
+}
+
+
+export function formatCurrency ( value: number) { 
+  return Intl.NumberFormat('en-US', {
+    style: 'currency',
+    currency: 'INR',
+    minimumFractionDigits: 2,
+  }).format(value)
 }
