64ed94762f9cd98ed4e0406508ce0ba9b8b79aa5
Suman Mondal <mondalsumanrock20@gmail.com>
2024-05-18 17:21:43 +0530

    Data card and dat grid completed to show, remaining income and expenses to the overview page

--- components/dashbaord/data-card.tsx
+++ components/dashbaord/data-card.tsx
@@ -1,9 +1,18 @@
-import { cn } from "@/lib/utils";
+import { cn, formatCurrency, formatPercentage } from "@/lib/utils";
 import { IconType } from "react-icons";
 import { VariantProps, cva } from "class-variance-authority";
-import { Card, CardDescription, CardHeader, CardTitle } from "../ui/card";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "../ui/card";
 
-const boxVariant = cva("rounded-md p-3", {
+import { CountUp } from "@/components/ui/count-up";
+import { Skeleton } from "../ui/skeleton";
+
+const boxVariant = cva("rounded-md p-3 shrink-0", {
   variants: {
     variant: {
       default: "bg-blue-500/20",
@@ -11,9 +20,9 @@ const boxVariant = cva("rounded-md p-3", {
       danger: "bg-rose-500/20",
       warning: "bg-yellow-500/20",
     },
-    defaultVariants: {
-      variant: "default",
-    },
+  },
+  defaultVariants: {
+    variant: "default",
   },
 });
 
@@ -25,9 +34,9 @@ const iconVariant = cva("size-6", {
       danger: "fill-rose-500",
       warning: "fill-yellow-500",
     },
-    defaultVariants: {
-      variant: "default",
-    },
+  },
+  defaultVariants: {
+    variant: "default",
   },
 });
 
@@ -47,16 +56,63 @@ export const DataCard = ({
   title,
   value = 0,
   dateRange,
+  variant,
   percentageChange = 0,
 }: DatacardProps) => {
   return (
     <Card className="border-none drop-shadow-sm">
       <CardHeader className="flex flex-row items-center justify-between gap-x-4">
-       <div className="space-y-2">
-       <CardTitle className="text-xl line-clamp-1">{title}</CardTitle>
-        <CardDescription className="line-clamp-1">{dateRange}</CardDescription>
-       </div>
+        <div className="space-y-2">
+          <CardTitle className="text-xl line-clamp-1">{title}</CardTitle>
+          <CardDescription className="line-clamp-1">
+            {dateRange}
+          </CardDescription>
+        </div>
+        <div className={cn(boxVariant({ variant }))}>
+          <Icon className={cn(iconVariant({ variant }))} />
+        </div>
       </CardHeader>
+      <CardContent>
+        <h1 className="font-semibold text-2xl mb-2 line-clamp-1 break-all">
+          <CountUp
+            preserveValue
+            start={0}
+            end={value}
+            decimals={2}
+            decimalPlaces={2}
+            duration={1}
+            formattingFn={formatCurrency}
+          />
+        </h1>
+        <p
+          className={cn(
+            "text-muted-foreground text-sm line-clamp-1",
+            percentageChange > 0 && "text-emerald-500",
+            percentageChange < 0 && "text-rose-500"
+          )}
+        >
+          {formatPercentage(percentageChange, { addPrefix: true })} from last
+          period
+        </p>
+      </CardContent>
+    </Card>
+  );
+};
+
+export const DataCardLoading = () => {
+  return (
+    <Card className="border-none drop-shadow-sm h-[192px]">
+      <CardHeader className="flex flex-row items-center justify-between gap-x-4">
+        <div className="space-y-2">
+          <Skeleton className="w-24 h-6" />
+          <Skeleton className="w-40 h-4" />
+        </div>
+        <Skeleton className="size-12" />
+      </CardHeader>
+      <CardContent>
+        <Skeleton className="shrink-0 h-10 w-24 mb-2" />
+        <Skeleton className="shrink-0 h-4 w-40" />
+      </CardContent>
     </Card>
   );
 };
--- components/dashbaord/data-grid.tsx
+++ components/dashbaord/data-grid.tsx
@@ -2,28 +2,54 @@
 import { useGetSummary } from "@/features/summary/api/use-get-summary";
 import { formatDateRange } from "@/lib/utils";
 import { FaPiggyBank } from "react-icons/fa";
+import { FaArrowTrendUp, FaArrowTrendDown } from "react-icons/fa6";
 import { useSearchParams } from "next/navigation";
-import { DataCard } from "./data-card";
-
+import { DataCard, DataCardLoading } from "./data-card";
 
 export const DataGrid = () => {
-    const {data} = useGetSummary();
+  const { data, isLoading } = useGetSummary();
   const params = useSearchParams();
   const to = params.get("to") || undefined;
   const from = params.get("from") || undefined;
 
   const dateRangeLabel = formatDateRange({ to, from });
 
+  if(isLoading){
+    return (
+        <div className="grid grid-cols-1 lg:grid-cols-3 gap-8 pb-2 mb-5">
+            <DataCardLoading/>
+            <DataCardLoading/>
+            <DataCardLoading/>
+            </div>
+    )
+  }
+
   return (
     <div className="grid grid-cols-1 lg:grid-cols-3 gap-8 pb-2 mb-5">
-     <DataCard
-     title="Remaining Balance"
-     value={data?.remainingAmount}
-     percentageChange={data?.remainingChange}
-     icon={FaPiggyBank}
-     variant="default"
-     dateRange={dateRangeLabel}
-     />
+      <DataCard
+        title="Remaining"
+        value={data?.remainingAmount}
+        percentageChange={data?.remainingChange}
+        icon={FaPiggyBank}
+        variant="default"
+        dateRange={dateRangeLabel}
+      />
+      <DataCard
+        title="Income"
+        value={data?.incomeAmount}
+        percentageChange={data?.incomeChange}
+        icon={FaArrowTrendUp}
+        variant="default"
+        dateRange={dateRangeLabel}
+      />
+      <DataCard
+        title="Expenses"
+        value={data?.expensesAmount}
+        percentageChange={data?.expensesChange}
+        icon={FaArrowTrendDown}
+        variant="default"
+        dateRange={dateRangeLabel}
+      />
     </div>
   );
 };
